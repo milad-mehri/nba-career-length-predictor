1bc1c99e4c27b0129698ad57f9cda801e9ebecaf
Milad Mehri <memm.milad@gmail.com>
2024-06-30 02:01:55 -0700

    Frontend

--- ai_model/scripts/predict.py
+++ ai_model/scripts/predict.py
@@ -2,23 +2,22 @@ import pandas as pd
 import joblib
 
 def predict_career_length():
-    current_players = pd.read_csv('./data/current_players.csv')
     nba_data_summary = pd.read_csv('./data/nba_data_summary.csv')
     
     career_length_model = joblib.load('./models/career_length_model.pkl')
     
-
     feature_columns = ['PLAYER_AGE', 'GP', 'PPG', 'APG', 'RPG', 'SPG', 'TOV', 'PPG_TREND', 'RPG_TREND', 'APG_TREND', 'MPG_TREND', 'AVG_GAMES_MISSED']
     
-    current_players_features = current_players[feature_columns]
-    career_length_predictions = career_length_model.predict(current_players_features)
+    if 'AVG_GAMES_MISSED' not in nba_data_summary.columns:
+        nba_data_summary['AVG_GAMES_MISSED'] = 0
+
+    all_players_features = nba_data_summary[feature_columns]
+    career_length_predictions = career_length_model.predict(all_players_features)
     
-    current_players['Predicted_Career_Length'] = career_length_predictions
+    nba_data_summary['Predicted_Career_Length'] = career_length_predictions
     
-    current_players = current_players.merge(nba_data_summary[['PLAYER_ID', 'CAREER_LENGTH']], on='PLAYER_ID', how='left')
-    
-    current_players.to_csv('./data/current_players_with_predictions.csv', index=False)
-    print("Predictions saved to './data/current_players_with_predictions.csv'")
+    nba_data_summary.to_csv('./data/all_players_with_predictions.csv', index=False)
+    print("Predictions saved to './data/all_players_with_predictions.csv'")
 
 if __name__ == '__main__':
     predict_career_length()
---
+++ frontend/public/script.js
@@ -0,0 +1,167 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const searchBar = document.getElementById('search-bar');
+    const sortOptions = document.getElementById('sort-options');
+    const statusFilter = document.getElementById('status-filter');
+    const playerList = document.getElementById('player-list');
+    const playerContainer = document.getElementById('player-container');
+    const loadingContainer = document.getElementById('loading');
+    const firstPageButton = document.getElementById('first-page');
+    const placeholderImage = 'https://via.placeholder.com/50';
+    const itemsPerPage = 50;
+    let currentPage = 1;
+    let currentPlayers = [];
+
+    fetch('./frontend/data/players_combined.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+            return response.json();
+        })
+        .then(players => {
+            currentPlayers = players;
+            sortAndDisplayPlayers();
+
+            searchBar.addEventListener('input', () => {
+                const searchTerm = searchBar.value.toLowerCase();
+                filterAndDisplayPlayers(searchTerm, statusFilter.value);
+            });
+
+            sortOptions.addEventListener('change', () => {
+                currentPage = 1;
+                sortAndDisplayPlayers();
+            });
+
+            statusFilter.addEventListener('change', () => {
+                currentPage = 1;
+                filterAndDisplayPlayers(searchBar.value.toLowerCase(), statusFilter.value);
+            });
+
+            document.getElementById('next-page').addEventListener('click', () => {
+                if ((currentPage * itemsPerPage) < currentPlayers.length) {
+                    currentPage++;
+                    displayPlayers(currentPlayers, currentPage);
+                }
+            });
+
+            document.getElementById('prev-page').addEventListener('click', () => {
+                if (currentPage > 1) {
+                    currentPage--;
+                    displayPlayers(currentPlayers, currentPage);
+                }
+            });
+
+            firstPageButton.addEventListener('click', () => {
+                currentPage = 1;
+                displayPlayers(currentPlayers, currentPage);
+            });
+        })
+        .catch(error => console.error('Error fetching player data:', error))
+        .finally(() => {
+            loadingContainer.classList.add('hidden');
+          loadingContainer.classList.remove('loading-container');
+
+            playerContainer.classList.remove('hidden');
+        });
+
+    function filterAndDisplayPlayers(searchTerm, status) {
+        currentPlayers = players.filter(player => {
+            const matchesSearchTerm = player.full_name.toLowerCase().includes(searchTerm);
+            const matchesStatus = (status === 'all') || (status === 'active' && player.is_active) || (status === 'inactive' && !player.is_active);
+            return matchesSearchTerm && matchesStatus;
+        });
+        currentPage = 1;
+        sortAndDisplayPlayers();
+    }
+
+    function sortAndDisplayPlayers() {
+        const sortValue = sortOptions.value;
+        if (sortValue === 'ppg') {
+            currentPlayers.sort((a, b) => b.PPG - a.PPG);
+        } else if (sortValue === 'apg') {
+            currentPlayers.sort((a, b) => b.APG - a.APG);
+        } else if (sortValue === 'rpg') {
+            currentPlayers.sort((a, b) => b.RPG - a.RPG);
+        } else if (sortValue === 'accuracy') {
+            currentPlayers.sort((a, b) => Math.abs(a.CAREER_LENGTH - a.Predicted_Career_Length) - Math.abs(b.CAREER_LENGTH - b.Predicted_Career_Length));
+        } else if (sortValue === 'positive-dif') {
+            currentPlayers.sort((a, b) => (b.Predicted_Career_Length - b.CAREER_LENGTH) - (a.Predicted_Career_Length - a.CAREER_LENGTH));
+        } else if (sortValue === 'negative-dif') {
+            currentPlayers.sort((a, b) => (a.Predicted_Career_Length - a.CAREER_LENGTH) - (b.Predicted_Career_Length - b.CAREER_LENGTH));
+        }
+        displayPlayers(currentPlayers, currentPage);
+    }
+
+  function displayPlayers(players, page) {
+      playerList.innerHTML = '';
+      const start = (page - 1) * itemsPerPage;
+      const end = start + itemsPerPage;
+      const paginatedPlayers = players.slice(start, end);
+
+      paginatedPlayers.forEach(player => {
+          const listItem = document.createElement('li');
+          listItem.className = 'player-item';
+
+          const img = document.createElement('img');
+          img.src = `${player.headshot_url}?v=${new Date().getTime()}`;
+          img.alt = player.full_name;
+          img.onerror = () => img.src = placeholderImage;
+
+          const infoDiv = document.createElement('div');
+          infoDiv.className = 'player-info';
+
+          const nameSpan = document.createElement('span');
+          nameSpan.className = 'player-name';
+          nameSpan.textContent = player.full_name;
+
+          const statusSpan = document.createElement('span');
+          statusSpan.className = `player-status ${player.is_active ? 'active' : 'inactive'}`;
+          statusSpan.textContent = player.is_active ? 'Active' : 'Inactive';
+
+          const statsDiv = document.createElement('div');
+          statsDiv.className = 'player-stats';
+
+          const seasonsSpan = document.createElement('span');
+          seasonsSpan.textContent = `Seasons: ${player.CAREER_LENGTH.toFixed(2)}`;
+
+          const ppgSpan = document.createElement('span');
+          ppgSpan.textContent = `PPG: ${player.PPG.toFixed(2)}`;
+
+          const apgSpan = document.createElement('span');
+          apgSpan.textContent = `APG: ${player.APG.toFixed(2)}`;
+
+          const rpgSpan = document.createElement('span');
+          rpgSpan.textContent = `RPG: ${player.RPG.toFixed(2)}`;
+
+          const predictedSpan = document.createElement('span');
+          predictedSpan.textContent = `AI Predicted Seasons: ${player.Predicted_Career_Length.toFixed(2)}`;
+
+          if (player.is_active) {
+              if (player.Predicted_Career_Length < player.CAREER_LENGTH) {
+                  predictedSpan.className = 'stat-red';
+              } else {
+                  predictedSpan.className = 'stat-yellow';
+              }
+          } else {
+              if (player.Predicted_Career_Length < player.CAREER_LENGTH) {
+                  predictedSpan.className = 'stat-red';
+              } else {
+                  predictedSpan.className = 'stat-green';
+              }
+          }
+
+          statsDiv.append(seasonsSpan, ppgSpan, apgSpan, rpgSpan, predictedSpan);
+          infoDiv.append(img, nameSpan, statusSpan);
+          listItem.append(infoDiv, statsDiv);
+          playerList.appendChild(listItem);
+      });
+
+      document.getElementById('page-number').textContent = page;
+      if (currentPage === 1) {
+          firstPageButton.classList.add('hidden');
+      } else {
+          firstPageButton.classList.remove('hidden');
+      }
+  }
+
+});
